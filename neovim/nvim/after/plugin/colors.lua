90dbc2730104b5a0394b913e3728d7774a537f85
Peter Sideris <petersid2022@gmail.com>
2023-10-09 13:15:56 +0300

    fixed folder structure

--- neovim/nvim/after/plugin/colors.lua
+++
@@ -1,6 +0,0 @@
---vim.o.background = "light" -- or "dark" for light mode
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
---vim.cmd.colorscheme "catppuccin-latte"
---vim.cmd.colorscheme "catppuccin-frappe"
---vim.cmd[[colorscheme tokyonight]]
