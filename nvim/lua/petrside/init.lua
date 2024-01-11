17bd272b462aeedb7dfd748ac264b1d25c011279
Peter Sideris <petersid2022@gmail.com>
2024-01-11 12:17:18 +0200

    fixed packer being installed alongside and setting up lazy

--- nvim/lua/petrside/init.lua
+++
@@ -1,8 +0,0 @@
-require("petrside.remap")
-require("petrside.set")
-require("petrside.lazy")
-
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
-vim.wo.sidescrolloff = 40
