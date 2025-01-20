1535a9e3450dfee7a8765a862144b24a9794e3fe
Peter Sideris <petersid2022@gmail.com>
2025-01-20 18:11:29 +0200

    autocmds should be at the autocmds file

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,34 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("custom-lsp-attach", { clear = true }),
-      callback = function(event)
-        local clients = vim.lsp.get_clients()
-        for _, client in ipairs(clients) do
-          vim.lsp.completion.enable(true, client.id, 0, { autotrigger = true })
-          vim.keymap.set("n", "<leader>hh", function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-          end)
-        end
-
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = event.buf, remap = false })
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = event.buf, remap = false })
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = event.buf, remap = false })
-        vim.keymap.set("n", "<leader>ac", vim.lsp.buf.code_action, { buffer = event.buf, remap = false })
-        vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { buffer = event.buf, remap = false })
-        vim.keymap.set("n", "<leader>ll", vim.lsp.buf.references, { buffer = event.buf, remap = false })
-        vim.keymap.set("n", "I", vim.diagnostic.open_float, { buffer = event.buf, remap = false })
-        vim.keymap.set("n", "<leader>d", vim.diagnostic.setloclist, { buffer = event.buf, remap = false })
-        vim.keymap.set("n", "<leader>wd", vim.diagnostic.setqflist, { buffer = event.buf, remap = false })
-        vim.keymap.set("n", "<leader>fb", function()
-          vim.lsp.buf.format { async = true }
-        end, { buffer = event.buf, remap = false })
-      end
-    })
-
     local lspconfig = require "lspconfig"
-
     local servers = {
       lua_ls = {
         settings = {
